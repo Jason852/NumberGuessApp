2e191c483b92bd0763d49b939e54be0f08efaed8
Jason852 <62583852+Jason852@users.noreply.github.com>
2023-01-06 12:54:20 -0500

    Started building

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
+
+export default function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
